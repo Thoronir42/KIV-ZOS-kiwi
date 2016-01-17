800b3d0857a64d3792ac50ea5b70fc35c744f3b2
Thoronir42 <Thoronir42@Gmail.com>
2016-01-17 17:48:15 +0100

    Workspace preparation

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "fat-info.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	for(int i = 1; i < argc; i ++){
+		printf(argv[i]);
+	}
+	
+	return 0;
+}
