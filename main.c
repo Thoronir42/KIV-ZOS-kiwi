7ae3727e99dfe6edea1db76ee79a176d0c79c391
Thoronir42 <Thoronir42@Gmail.com>
2016-01-20 15:00:10 +0100

    Prepared argument handling

--- main.c
+++ main.c
@@ -3,10 +3,42 @@
 
 #include "fat-operator.h"
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int req_thread_count;
+
+int process_parameters(int argc, char *argv[]) {
+	int threads;
+	if (argc < 1) {
+		printf("Not enough paramaters\n");
+		return 2;
+	}
+	if (!strcmp("help", argv[0])) {
+		return 1;
+	}
+	threads = atoi(argv[0]);
+	if (threads < 1) {
+		printf("Number of threads parameter '%s' is not a number.\n", argv[0]);
+		return 3;
+	}
+	
+	req_thread_count = threads;
+
+	return 0;
+}
+
+int print_help() {
+	printf("Usage: \n");
+	printf("soubor [threads]\n");
+	printf("\t[threads]: amount of threads to be used for current run\n");
+	return 0;
+}
 
 int main(int argc, char *argv[]) {
-	main_read();
+	if (process_parameters(argc - 1, argv + 1)) {
+			print_help();
+			return 1;
+	}
+	printf("Pocet vlaken %d", req_thread_count);
+	//main_read();
 
 	return 0;
 }
