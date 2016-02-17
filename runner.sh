046c7b52da780c73424173b7ce4483ded759a486
Thoronir42 <Thoronir42@Gmail.com>
2016-02-17 20:42:59 +0100

    Fixed runner and expanded documentation

--- runner.sh
+++ runner.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+
+echo "threads;check;shake\n" > res_times.csv
 for(( i=1; i<=8; i++ ))
 do 
-	echo "threads;check;shake\n" > res_times.csv
-	echo "$i;" >> res_times.csv
-	{ ./FATty check $i>> res_times.csv ; }
-	echo ";" >> res_times.csv
-	{ ./FATty shake $i> res_times.csv ; }
-done
+	
+	printf "$i;"
+	./FATty check $i
+	printf ";"
+	./FATty shake $i;
+	echo ""
+done >> res_times.csv
